06cc22c05edfa61e36e7d805d13cae4db2d6d19f
lzlz <lzlz0810@qq.com>
2017-11-27 14:05:41 +0800

    重命名文件夹

--- game_model/Game.js
+++ game_model/Game.js
@@ -1,15 +1,21 @@
+const MAX_PLAYERS =20;
 var Game = function (starMap) {
+    var _playerCount = 0;
     var _players = {};
     this.starMap = starMap;
     this.__proto__ = {
         //new 一个player需要属性：id,name
         addPlayer: player => {
+            if(_playerCount>=MAX_PLAYERS)
+                return false;
             var star = this.starMap.getOwnerlessStar();
             if(star){
                 _players[player.getId()] = player;
                 player.addStar(star);
-                return this;
+                _playerCount++;
+                return true;
             }
+            return false;
         },
         getPlayer: playerID => _players[playerID],
     };
